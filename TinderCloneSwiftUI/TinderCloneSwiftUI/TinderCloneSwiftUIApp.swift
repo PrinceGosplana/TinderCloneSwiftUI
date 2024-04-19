6393a593fc21d79dc703acf0bdb79ad94d38ac39
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-19 16:46:08 +0300

    init

---
+++ TinderCloneSwiftUI/TinderCloneSwiftUI/TinderCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneSwiftUIApp.swift
+//  TinderCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
