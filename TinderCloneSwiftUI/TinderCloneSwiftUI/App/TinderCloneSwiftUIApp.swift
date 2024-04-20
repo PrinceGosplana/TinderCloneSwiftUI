517dbd636d98896823af267de6dd4895a3f96e3f
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-20 12:20:44 +0300

    add folders

---
+++ TinderCloneSwiftUI/TinderCloneSwiftUI/App/TinderCloneSwiftUIApp.swift
@@ -0,0 +1,29 @@
+//
+//  TinderCloneSwiftUIApp.swift
+//  TinderCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 19.04.2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct TinderCloneSwiftUIApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
