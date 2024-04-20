1f0f39121d23064655b2b78457525b2aa27881d5
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-20 12:36:28 +0300

    tab view and assets

---
+++ TinderCloneSwiftUI/TinderCloneSwiftUI/Core/TabBar/MainTabView.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabView.swift
+//  TinderCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 20.04.2024.
+//
+
+import SwiftUI
+
+enum TabbarType: Int {
+    case swiping, search, inbox, profile
+}
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Swiping View")
+                .tabItem { Image(systemName: "flame") }
+                .tag(TabbarType.swiping)
+
+            Text("Search View")
+                .tabItem { Image(systemName: "magnifyingglass") }
+                .tag(TabbarType.search)
+
+            Text("Inbox View")
+                .tabItem { Image(.messagesIcon)
+                        .renderingMode(.template)
+                }
+                .tag(TabbarType.inbox)
+
+            Text("Profile View")
+                .tabItem { Image(systemName: "person") }
+                .tag(TabbarType.profile)
+        }
+        .tint(.primary)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
