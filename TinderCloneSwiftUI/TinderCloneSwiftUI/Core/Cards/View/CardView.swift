13ca2a6bc19161d36dda9ceab9d00c295f025539
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-21 09:28:42 +0300

    add functionality for showing several images

--- TinderCloneSwiftUI/TinderCloneSwiftUI/Core/Cards/View/CardView.swift
+++ TinderCloneSwiftUI/TinderCloneSwiftUI/Core/Cards/View/CardView.swift
@@ -11,14 +11,21 @@ struct CardView: View {
 
     @State private var xOffset: CGFloat = 0
     @State private var degrees: Double = 0
+    @State private var currentImageIndex = 0
+
+    @State private var mockImages = [
+        "megan-fox-1", "megan-fox-2"
+    ]
 
     var body: some View {
         ZStack(alignment: .bottom) {
             ZStack(alignment: .top) {
-                Image(.meganFox1)
+                Image(mockImages[currentImageIndex])
                     .resizable()
                 .scaledToFill()
-
+                .overlay {
+                    ImageScrollingOverlay(currentImageIndex: $currentImageIndex, imageCount: mockImages.count)
+                }
                 SwipeActionIndicator(xOffset: $xOffset)
             }
 
