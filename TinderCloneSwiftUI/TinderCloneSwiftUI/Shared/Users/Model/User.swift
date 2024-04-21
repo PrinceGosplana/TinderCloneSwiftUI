3998e30b3da89cf6fdbd2debe95ad52148fb1c9a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-21 16:01:16 +0300

    add user model

---
+++ TinderCloneSwiftUI/TinderCloneSwiftUI/Shared/Users/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  TinderCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 21.04.2024.
+//
+
+
+struct User: Identifiable {
+    let id: String
+    let fullName: String
+    var age: Int
+    var profileImageURLs: [String]
+}
